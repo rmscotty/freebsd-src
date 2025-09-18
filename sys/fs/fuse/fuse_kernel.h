a219149fcac890ab8c7b3c4839583ae975fd6f38
Claudiu I. Palincas <mscotty@protonmail.ch>
2025-09-18 23:35:32 +0100

    fusefs: Upgrade FUSE protocol to version 7.37.
        This commit upgrades the FUSE API to protocol 7.37,
        it doesn't implement any of protocol 7.37's new features,

--- sys/fs/fuse/fuse_kernel.h
+++ sys/fs/fuse/fuse_kernel.h
@@ -195,10 +195,13 @@
  *  - add FUSE_SECURITY_CTX init flag
  *  - add security context to create, mkdir, symlink, and mknod requests
  *  - add FUSE_HAS_INODE_DAX, FUSE_ATTR_DAX
+ *
+ *  7.37
+ *  - add FUSE_TMPFILE
  */
 
-#ifndef _FUSE_FUSE_KERNEL_H
-#define _FUSE_FUSE_KERNEL_H
+#ifndef _LINUX_FUSE_H
+#define _LINUX_FUSE_H
 
 #ifdef __linux__
 #include <linux/types.h>
@@ -230,7 +233,7 @@
 #define FUSE_KERNEL_VERSION 7
 
 /** Minor version number of this interface */
-#define FUSE_KERNEL_MINOR_VERSION 36
+#define FUSE_KERNEL_MINOR_VERSION 37
 
 /** The node ID of the root inode */
 #define FUSE_ROOT_ID 1
@@ -546,6 +549,7 @@ enum fuse_opcode {
 	FUSE_SETUPMAPPING	= 48,
 	FUSE_REMOVEMAPPING	= 49,
 	FUSE_SYNCFS		= 50,
+	FUSE_TMPFILE		= 51,
 
 #ifdef linux
 	/* CUSE specific operations */
